9aed7b9f95cb18e49a1f92a23cbd98bd803efc87
Maulen <azikulov@proton.me>
2026-09-21 23:44:02 +0500

    feat: добавлен интерфейс IUser и репозиторий StoreRepository для управления пользователями

---
+++ backend/app/modules/users/users.repository.ts
@@ -0,0 +1,23 @@
+import type { Pool } from "pg";
+import { ICreateUserDto, IPublicUser } from "./users.interface";
+
+export class StoreRepository {
+  constructor(private readonly db: Pool) {}
+
+  async create(dto: ICreateUserDto): Promise<IPublicUser> {
+    const response = await this.db.query<IPublicUser>(
+      `INSERT INTO users (
+        email, first_name, last_name, password
+      )
+      VALUES (
+        $1, $2
+      )
+      RETURNING
+        id, email, first_name, last_name, created_at, updated_at;
+    `,
+      [dto.email, dto.first_name, dto.last_name, dto.password],
+    );
+
+    return response.rows[0];
+  }
+}
