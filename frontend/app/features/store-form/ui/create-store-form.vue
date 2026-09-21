21b5df9af2f3e6fba2d8a2bb747db65582ca1527
Maulen <azikulov@proton.me>
2026-09-21 17:40:19 +0500

    feat: добавлены компоненты и логика для создания формы магазина, включая обработку ввода и отправку данных

---
+++ frontend/app/features/store-form/ui/create-store-form.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { BaseButton, BaseInput } from "~/shared/ui";
+import { useCreateStoreForm } from "../composables/useCreateStoreForm";
+
+const { form, isSubmitting, submit } = useCreateStoreForm();
+</script>
+
+<template>
+  <form class="space-y-4" @submit.prevent="submit">
+    <BaseInput
+      v-model="form.company"
+      label="Компания"
+      placeholder="Введите название компании"
+    />
+
+    <BaseButton
+      class="w-full"
+      :type="'submit'"
+      variant="primary"
+      :loading="isSubmitting"
+    >
+      Создать компанию
+    </BaseButton>
+  </form>
+</template>
